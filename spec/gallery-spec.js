6ab853147fcf8bf9b93445708fb8eabb255ecc07
Joseph Wynn <wildlyinaccurate@users.noreply.github.com>
2016-05-29 02:22:41 +0100

    Gallery implementation (#3)
    * Implement a very basic implementation of gallery()
    
    * Add test for gallery()

---
+++ spec/gallery-spec.js
@@ -0,0 +1,56 @@
+/* eslint handle-callback-err: "off" */
+const fs = require('fs')
+const rimraf = require('rimraf')
+const proxyquire = require('proxyquire')
+
+const templateSpy = jasmine.createSpy('template').and.returnValue('MOCK HTML')
+
+const gallery = proxyquire('../src/gallery', {
+  'lodash': {
+    template: () => templateSpy
+  }
+})
+
+const mockDiff = percentage => {
+  return {
+    results: { percentage },
+    image: {},
+    base: {},
+    test: {}
+  }
+}
+
+describe('gallery()', () => {
+  it('should order results by highest difference', done => {
+    const diff1 = mockDiff(0.08)
+    const diff2 = mockDiff(0.1)
+
+    fs.mkdtemp('/tmp/whoopsie-', (err, dir) => {
+      gallery(dir, [diff1, diff2], 10)
+        .then(() => {
+          const results = templateSpy.calls.argsFor(0)[0].results
+
+          expect(results).toEqual([diff2, diff1])
+
+          rimraf(dir, done)
+        })
+    })
+  })
+
+  it('should correctly identify failures based on the threshold', done => {
+    const diff1 = mockDiff(0.08)
+    const diff2 = mockDiff(0.1)
+
+    fs.mkdtemp('/tmp/whoopsie-', (err, dir) => {
+      gallery(dir, [diff1, diff2], 10)
+        .then(() => {
+          const results = templateSpy.calls.argsFor(0)[0].results
+
+          expect(results[0].failed).toBe(true)
+          expect(results[1].failed).toBe(false)
+
+          rimraf(dir, done)
+        })
+    })
+  })
+})
---
+++ src/gallery.js
@@ -0,0 +1,53 @@
+const _ = require('lodash')
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs'))
+const mkdirp = Promise.promisify(require('mkdirp'))
+const path = require('path')
+const crypto = require('crypto')
+
+module.exports = function gallery (baseDir, diffs, failureThreshold) {
+  const galleryDir = path.join(baseDir, galleryName())
+
+  return fs.readFileAsync(templatePath(), 'utf8')
+    .then(_.template)
+    .then(template => {
+      const results = processDiffs(diffs, failureThreshold)
+
+      return template({
+        results,
+        summary: makeSummary(results)
+      })
+    })
+    .then(html => [html, mkdirp(galleryDir)])
+    .all()
+    .then(([html]) => fs.writeFileAsync(path.join(galleryDir, 'index.html'), html))
+}
+
+function templatePath () {
+  return path.join(__dirname, 'template/gallery.html')
+}
+
+function makeSummary (diffs) {
+  const total = diffs.length
+  const failures = _.filter(diffs, 'failed').length
+  const passes = total - failures
+
+  return { total, failures, passes }
+}
+
+function processDiffs (diffs, failureThreshold) {
+  const annotated = diffs.map(diff => {
+    diff.failed = diff.results.percentage >= (failureThreshold / 100)
+
+    return diff
+  })
+
+  return _.orderBy(annotated, 'results.percentage', 'desc')
+}
+
+function galleryName () {
+  return crypto.createHash('sha1')
+    .update(new Date().toJSON())
+    .digest('hex')
+    .substr(0, 12)
+}
