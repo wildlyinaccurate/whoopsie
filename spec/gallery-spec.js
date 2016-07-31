052d45ae163511cc27b9ab03134a12e4caa5eb55
Joseph Wynn <joseph@wildlyinaccurate.com>
2016-07-31 09:04:22 +0100

    Add test scenarios for failed compares

--- spec/gallery-spec.js
+++ spec/gallery-spec.js
@@ -21,6 +21,10 @@ const mockDiff = percentage => {
 }
 
 describe('gallery()', () => {
+  beforeEach(() => {
+    templateSpy.calls.reset()
+  })
+
   it('should order results by highest difference', done => {
     const diff1 = mockDiff(0.08)
     const diff2 = mockDiff(0.1)
@@ -48,6 +52,22 @@ describe('gallery()', () => {
 
           expect(results[0].failed).toBe(true)
           expect(results[1].failed).toBe(false)
+
+          rimraf(dir, done)
+        })
+    })
+  })
+
+  it('should ignore failed diffs', done => {
+    const diff1 = undefined
+    const diff2 = mockDiff(0.1)
+
+    fs.mkdtemp('/tmp/whoopsie-', (err, dir) => {
+      gallery(dir, [diff1, diff2], 10)
+        .then(() => {
+          const results = templateSpy.calls.argsFor(0)[0].results
+
+          expect(results).toEqual([diff2])
 
           rimraf(dir, done)
         })
