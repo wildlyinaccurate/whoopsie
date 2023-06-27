4019731c699e6fb81b287a1c504e8f934a658232
Joseph Wynn <joseph@wildlyinaccurate.com>
2023-06-27 16:38:16 +0200

    Fix log call

--- src/reporters/json.js
+++ src/reporters/json.js
@@ -2,5 +2,5 @@ const fs = require("fs-extra");
 const { log } = require("../log");
 
 module.exports = async function jsonReporter(output, config) {
-  return fs.outputJSON(config.inFile, output).then(() => log.write(`Wrote results to ${config.inFile}`));
+  return fs.outputJSON(config.inFile, output).then(() => log.notice(`Wrote results to ${config.inFile}`));
 };
