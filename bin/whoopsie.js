5da168a93d0c1af391a0fb5e7b5fe08884b39b55
Joseph Wynn <joseph@wildlyinaccurate.com>
2023-06-27 16:20:51 +0200

    Run ESLint on bin/whoopsie.js

--- bin/whoopsie.js
+++ bin/whoopsie.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const cli = require('../src/cli')
-const argv = require('minimist')(process.argv.slice(2))
+const cli = require("../src/cli");
+const argv = require("minimist")(process.argv.slice(2));
 
-cli(argv)
+cli(argv);
